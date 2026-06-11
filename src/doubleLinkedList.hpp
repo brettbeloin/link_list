8a30ace03e7a0d2f82652b758d1b1aec66b96784
brett beloin <brettbeloin@gmail.com>
2026-06-11 06:01:01 -0600

    setting up for doubleLinkList

---
+++ src/doubleLinkedList.hpp
@@ -0,0 +1,223 @@
+#pragma once
+#include <print>
+#include <sstream>
+#include <stdexcept>
+#include <string>
+
+template <typename T> class singleLinkedList {
+private:
+  struct Node {
+    T data;
+    Node *next;
+    Node(T value) : data(value), next(nullptr) {}
+  };
+
+  Node *head;
+  Node *tail;
+  int size;
+
+public:
+  singleLinkedList() : head(nullptr), tail(nullptr), size(0) {}
+  ~singleLinkedList() {
+    Node *node = head;
+    Node *temp;
+
+    for (int i = 0; i <= size - 1; i++) {
+      temp = node->next;
+      delete node;
+      node = temp;
+    }
+
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+  }
+
+  void Add(T val) {
+    Node *new_node = new Node(val);
+    if (!head) {
+      head = tail = new_node;
+    } else {
+      tail->next = new_node;
+      tail = new_node;
+    }
+    size++;
+  }
+
+  void Insert(T val, int index) {
+    if (index < 0 || index >= size) {
+      throw std::out_of_range("Index out of bounds");
+    }
+
+    Node *node = head;
+    Node *new_node = new Node(val);
+
+    if (index == 0) {
+      head = new_node;
+      new_node->next = node;
+      size++;
+      return;
+    }
+
+    for (int i = 0; i < index - 1; i++) {
+      node = node->next;
+    }
+
+    new_node->next = node->next;
+    node->next = new_node;
+
+    size++;
+  }
+
+  void Clear() {
+    Node *node = head;
+    Node *temp;
+
+    for (int i = 0; i <= size - 1; i++) {
+      temp = node->next;
+      delete node;
+      node = temp;
+    }
+
+    head = nullptr;
+    tail = nullptr;
+    size = 0;
+  }
+
+  int Search(T val) {
+    if (size == 0) {
+      throw std::out_of_range("The List is empty");
+    }
+
+    Node *node = head;
+
+    for (int i = 0; i <= size - 1; i++) {
+      if (node->data == val) {
+        return i;
+      }
+      node = node->next;
+    }
+
+    return -1;
+  }
+
+  int Count() { return size; }
+
+  T Get(int index) {
+    if (index < 0 || index >= size) {
+      throw std::out_of_range("Index out of bounds");
+    }
+
+    Node *curr_node = head;
+    for (int i = 0; i < index; i++) {
+      curr_node = curr_node->next;
+    }
+
+    return curr_node->data;
+  }
+
+  T Remove() {
+    if (size == 0) {
+      throw std::out_of_range("The List is empty");
+    }
+
+    Node *node = head;
+    T removed_val = node->data;
+    head = node->next;
+
+    if (size == 1) {
+      tail = head;
+    }
+
+    delete node;
+
+    size--;
+    return removed_val;
+  }
+
+  T RemoveAt(int index) {
+    if (index < 0 || index >= size) {
+      throw std::out_of_range("Index out of bounds");
+    }
+
+    Node *node = head;
+    Node *temp;
+
+    if (index == 0) {
+      T removed_val = node->data;
+      head = node->next;
+      delete node;
+      size--;
+      return removed_val;
+    }
+
+    for (int i = 0; i < index - 1; i++) {
+      node = node->next;
+    }
+
+    if (index == size - 1) {
+      T removed_val = node->next->data;
+      delete node->next;
+      tail = node;
+      size--;
+      return removed_val;
+    }
+
+    temp = node->next->next;
+    T removed_val = node->next->data;
+    delete node->next;
+    node->next = temp;
+    size--;
+
+    return removed_val;
+  }
+
+  T RemoveLast() {
+    if (size == 0) {
+      throw std::out_of_range("The List is empty");
+    }
+
+    Node *node = head;
+
+    if (size == 1) {
+      T removed_val = node->data;
+      delete node;
+      head = nullptr;
+      tail = nullptr;
+      size--;
+      return removed_val;
+    }
+
+    for (int i = 0; i < size - 2; i++) {
+      node = node->next;
+    }
+
+    T removed_val = node->next->data;
+    delete node->next;
+    tail = node;
+    size--;
+
+    return removed_val;
+  }
+
+  std::string ToString() {
+    if (size == 0) {
+      return "";
+    }
+
+    std::ostringstream oss;
+    Node *node = head;
+
+    for (int i = 0; i <= size - 1; i++) {
+      oss << node->data;
+
+      if (i < size - 1) {
+        oss << ", ";
+      }
+
+      node = node->next;
+    }
+
+    return oss.str();
+  }
+};
--- tests/double_test.cpp
+++ tests/double_test.cpp
@@ -1,1 +1,233 @@
-#include "../external/doctest/doctest/doctest.h"
+#include "doubleLinkedList.hpp"
+#include <../external/doctest/doctest/doctest.h>
+#include <stdexcept>
+
+TEST_CASE("Testing The Add method") {
+  SUBCASE("Testing a single element") {
+    singleLinkedList<int> list;
+
+    CHECK(list.Count() == 0);
+    list.Add(75);
+    CHECK(list.Count() == 1);
+  }
+
+  SUBCASE("Testing multiple elements with generics") {
+    singleLinkedList<std::string> list;
+
+    CHECK(list.Count() == 0);
+    list.Add("Hello");
+    list.Add("World");
+    list.Add("!");
+    CHECK(list.Count() == 3);
+  }
+}
+
+TEST_CASE("Testing The Get method") {
+  singleLinkedList<int> list;
+
+  list.Add(1);
+  list.Add(3);
+  list.Add(7);
+  list.Add(6);
+  list.Add(2);
+
+  SUBCASE("Test get proper value") {
+    int correct_number = list.Get(3);
+    CHECK(correct_number == 6);
+  }
+
+  SUBCASE("Test get out of bounds err") {
+    CHECK_THROWS_AS(list.Get(5), std::out_of_range);
+    CHECK_THROWS_AS(list.Get(-1), std::out_of_range);
+  }
+}
+
+TEST_CASE("Testing the ToString method") {
+  singleLinkedList<int> list;
+
+  SUBCASE("Test empty list") { CHECK(list.ToString() == ""); }
+
+  SUBCASE("Test full list") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    list.Add(2);
+
+    CHECK(list.ToString() == "1, 3, 7, 6, 2");
+  }
+}
+
+TEST_CASE("Testing the Remove method") {
+  singleLinkedList<int> list;
+
+  SUBCASE("Happy Path") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    list.Add(2);
+    list.Remove();
+
+    CHECK(list.Count() == 4);
+  }
+
+  SUBCASE("Test Empty list") {
+    CHECK_THROWS_AS(list.Remove(), std::out_of_range);
+  }
+
+  SUBCASE("Test tail edge case") {
+    list.Add(3);
+    list.Remove();
+
+    CHECK(list.Count() == 0);
+  }
+}
+
+TEST_CASE("Testing the Clear method") {
+  singleLinkedList<int> list;
+
+  SUBCASE("Clear full list") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    list.Add(2);
+    list.Clear();
+
+    CHECK(list.Count() == 0);
+  }
+
+  SUBCASE("Clear empty list") {
+    list.Clear();
+
+    CHECK(list.Count() == 0);
+  }
+}
+
+TEST_CASE("Testing the RemoveLast method") {
+  singleLinkedList<int> list;
+
+  SUBCASE("Test Happy") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    list.RemoveLast();
+
+    CHECK(list.Count() == 3);
+    CHECK((list.Get(list.Count() - 1)) == 7);
+  }
+
+  SUBCASE("Test a single element list") {
+    list.Add(5);
+    list.RemoveLast();
+
+    CHECK(list.Count() == 0);
+  }
+
+  SUBCASE("Test empty list") {
+    CHECK_THROWS_AS(list.RemoveLast(), std::out_of_range);
+  }
+}
+
+TEST_CASE("Testing the Search Method") {
+  singleLinkedList<int> list;
+
+  SUBCASE("Test Happy path") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    int idx = list.Search(7);
+
+    CHECK(idx == 2);
+  }
+
+  SUBCASE("Test empty list") {
+    CHECK_THROWS_AS(list.Search(5), std::out_of_range);
+  }
+
+  SUBCASE("Test Element not found") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    CHECK(list.Search(5) == -1);
+  }
+}
+
+TEST_CASE("Testing the Insert Method") {
+  singleLinkedList<int> list;
+
+  SUBCASE("Test Happy") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    list.Insert(5, 3);
+
+    CHECK(list.Count() == 5);
+    CHECK(list.Get(3) == 5);
+  }
+
+  SUBCASE("Test empty") {
+    list.Add(6);
+    list.Insert(5, 0);
+    CHECK(list.Count() == 2);
+    CHECK(list.Get(0) == 5);
+  }
+
+  SUBCASE("Test Tail logic") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    list.Insert(5, 3);
+    CHECK(list.Count() == 5);
+    CHECK((list.Get(list.Count() - 1)) == 6);
+  }
+
+  SUBCASE("Test out_of_range") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+
+    CHECK_THROWS_AS(list.Insert(5, -1), std::out_of_range);
+    CHECK_THROWS_AS(list.Insert(5, list.Count() + 1), std::out_of_range);
+    SUBCASE("test out_of_range on empty list") {
+      list.Clear();
+      CHECK_THROWS_AS(list.Insert(5, 0), std::out_of_range);
+    }
+  }
+}
+
+TEST_CASE("Testing RemoveAt method") {
+  singleLinkedList<int> list;
+
+  SUBCASE("Test Happy Path") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+    int removed_number = list.RemoveAt(2);
+
+    CHECK(list.Count() == 3);
+    CHECK(removed_number == 7);
+  }
+
+  SUBCASE("Test out_of_range") {
+    list.Add(1);
+    list.Add(3);
+    list.Add(7);
+    list.Add(6);
+
+    CHECK_THROWS_AS(list.RemoveAt(-1), std::out_of_range);
+    CHECK_THROWS_AS(list.RemoveAt(list.Count() + 1), std::out_of_range);
+    SUBCASE("test out_of_range on empty list") {
+      list.Clear();
+      CHECK_THROWS_AS(list.RemoveAt(0), std::out_of_range);
+    }
+  }
+}
